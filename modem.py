75f22e815dd6782214f2569404da5f14919f5395
Ryan <ryanthetran@gmail.com>
2026-04-18 22:25:11 -0700

    file check for decode_wavfile

--- modem.py
+++ modem.py
@@ -1,3 +1,5 @@
+import os
+
 from scipy.io import wavfile
 
 def decode_wavfile(file_path: str) -> str:
@@ -9,8 +11,14 @@ def decode_wavfile(file_path: str) -> str:
     Returns:
         A string of the decoded WAV file.
     """
+    sample_rate, data = wavfile.read(file_path)
+    if sample_rate != 48000 or data.ndim > 1:
+        raise Exception(f"{file_path} should be a 48 kilosample per second 16-bit mono WAV file.")
     
-    return f"When we're done, we will decode {file_path}."
+    return f"{file_path} message: TO BE ADDED HERE"
 
 if __name__ == "__main__":
-    print(decode_wavfile("message.wav"))
+    try:
+        print(decode_wavfile("message.wav"))
+    except Exception as e:
+        print (e)
