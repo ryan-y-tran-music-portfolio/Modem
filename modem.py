df6ab8df287a1d3f793d4e69ba09a97accad1b91
Ryan <ryanthetran@gmail.com>
2026-04-18 22:13:19 -0700

    initial commit for decode_wavfile

--- modem.py
+++ modem.py
@@ -1,1 +1,16 @@
 from scipy.io import wavfile
+
+def decode_wavfile(file_path: str) -> str:
+    """Decode a WAV File via a simplified Bell 103 Modem Protocol
+    
+    Args:
+        file_path: Path to WAV file to decode.
+
+    Returns:
+        A string of the decoded WAV file.
+    """
+    
+    return f"When we're done, we will decode {file_path}."
+
+if __name__ == "__main__":
+    print(decode_wavfile("message.wav"))
